15ead63f1b023372064ab07c1074650c725f7a09
Jirawat Boonsumret <jirawat859@hotmail.com>
2018-04-02 20:42:13 +0700

    [update] เพิ่มการอ่านไฟล์ config.json ที่ get path มาจาก db

--- routes/temperatureControl.js
+++ routes/temperatureControl.js
@@ -1,48 +1,49 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const fs = require("fs");
+const http = require("http");
 const farm = mongoose.model("farm");
 
-var filePathJson;
-var filePath;
+let filePathJson;
+let filePath;
+let configFile;
+let minTemperature;
+let maxTemperature;
 
-async function getFarmData(farmIdIn){
-    var farmData = await farm.find({farmId: farmIdIn});
-    if(farmData){
-        filePathJson =  JSON.stringify(farmData);
-    }else{
+async function getFarmData(farmIdIn) {
+    var farmData = await farm.find({
+        farmId: farmIdIn
+    });
+    if (farmData) {
+        filePathJson = JSON.stringify(farmData);
+    } else {
         console.log('fail');
     }
 }
 
+function compareTemperature(filePath, currentTemp) {
+    configFile = JSON.parse(require('fs').readFileSync(String(filePath), 'utf8'));
+    minTemperature = configFile.minTemperature;
+    maxTemperature = configFile.maxTemperature;
+    let options = {
+        host: "",
+        path: ""
+    }
+    if (maxTemperature > currentTemp) {
+
+    } else {
+
+    }
+}
+
 router.post("/", (req, res) => {
-    const newFarm = {
-        farmId: 123456789,
-        farmName: "Proto X",
-        ownerName: "Hot",
-        ownerSurname: "Head",
-        ownerTel: "123-456-789",
-        ownerAddress: "UNKNOW",
-        configFilePath: "./conf/farm-1.json"
-    };
-
-    new farm(newFarm).save(function (err) {
-        if (!err) {
-            console.log("created");
-            res.sendStatus(200);
-        } else {
-            //TODO: return page with errors
-            return console.log(err);
-        }
-    });
-});
-
-router.get("/", (req, res) => {
-    async function getPathData(){
+    async function getPathData() {
         await getFarmData(123456789);
         const temp = JSON.parse(filePathJson);
         filePath = temp[0].configFilePath;
-        console.log("filePath: " + filePath)
+        console.log("filePath: " + filePath);
+        await compareTemperature(filePath, req.body.temperature);
         res.sendStatus(200);
     }
     getPathData();
