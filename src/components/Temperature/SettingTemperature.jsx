b425f28c455a0695e32378a62754a99ec8405ad3
pon32258 <pon1852@hotmail.com>
2018-04-19 11:36:57 +0700

    [Add] ทดลอง Grid materailize-ui เเละ ใช้ gridlayout รวมถึง paperwork

--- src/components/Temperature/SettingTemperature.jsx
+++ src/components/Temperature/SettingTemperature.jsx
@@ -1,16 +1,44 @@
 import React ,{Component} from 'react';
-import { withStyles,Grid } from 'material-ui';
+import { withStyles,Grid,Paper,Typography  } from 'material-ui';
 import Dropdown from '../../Utils/Dropdown';
+import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
 
-function SettingTemperature(){
+
+const styles = theme => ({
+    
+    root: theme.mixins.gutters({
+        paddingTop: 16,
+        paddingBottom: 16,
+        marginTop: theme.spacing.unit * 5,
+      }),
+  })
+  
+
+function SettingTemperature(props){
+    const{classes} = props
     return(
+
         <Grid container>
-         
-            <Dropdown /> >
-            <Dropdown /> >
-            
+         <Grid item xs={12} sm={12} md={12} ld={12} xl={12}>
+            <Paper className={classes.root} styles={styles}>
+                <Dropdown /> 
+                <Dropdown />
+                <Button vataint ="raised">
+                    Send value
+                </Button>
+            </Paper>
+            </Grid>
+
+          
         </Grid>
+    
     )
 }
 
-export default (SettingTemperature);
+SettingTemperature.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+
+export default withStyles(styles) (SettingTemperature);
