78feb882c847b0997a655dc420f26af0330fcc29
Jirawat Boonsumret <jirawat859@hotmail.com>
2018-06-28 20:54:16 +0700

    [update] fix problem that can not getTime from data

--- server/classes/WaterControl/WateringConfig.js
+++ server/classes/WaterControl/WateringConfig.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 let configFile;
 let existGreenHouseIndex;
+let writeFileStatus;
 
 export default class WateringConfig {
   constructor(req, res) {
@@ -19,36 +20,48 @@ export default class WateringConfig {
         status: 500,
         errorMessage: "เกิดข้อผิดพลาดในการตั้งค่าการให้น้ำอัตโนมัติ"
       });
-    } else {
-      getConfigFile();
-      let tempJson = {
-        greenHouseId: greenHouseId,
-        timeRanges: []
-      };
-      let wateringConfig = configFile.watering;
-      let tempArray = [];
-      if (!Object.keys(wateringConfig).length == 0) {
-        for (let index = 0; index < Object.keys(wateringConfig).length; index++) {
-          let temp = wateringConfig[index];
-          if (temp.greenHouseId == greenHouseId) {
-            existGreenHouseIndex = index;
-          }
+    }
+    console.log("[WateringConfigs] greenHouseId: " + greenHouseId);
+    getConfigFile();
+    let tempJson = {
+      greenHouseId: greenHouseId,
+      timeRanges: []
+    };
+    let wateringConfig = configFile.watering;
+    let tempArray = [];
+    if (!Object.keys(wateringConfig).length == 0) {
+      for (let index = 0; index < Object.keys(wateringConfig).length; index++) {
+        let temp = wateringConfig[index];
+        if (temp.greenHouseId == greenHouseId) {
+          existGreenHouseIndex = index;
+        } else {
+          existGreenHouseIndex = -1;
         }
       }
-      for (let index = 0; index < configTimeRanges.length; index++) {
-        let tempDate = configTimeRanges[index];
-        let tempTimeMills = tempDate.getTime();
-        tempJson.timeRanges.push(tempTimeMills);
-      }
-      if (typeof existGreenHouseIndex === "undefined") {
-        res.json({
-          status: 500,
-          errorMessage: "ไม่สามารถตั้งค่าการให้น้ำของโรงเรือนที่ระบุได้",
-        });
-        return;
-      }
-      configFile.watering[existGreenHouseIndex] = tempJson;
-      writeConfigFile(configFile, res);
+    }
+    for (let index = 0; index < configTimeRanges.length; index++) {
+      let tempDate = new Date(configTimeRanges[index]);
+      console.log("[WateringConfigs] configTimeRanges" + "[" + index + "]: " + configTimeRanges[index]);
+      let tempTimeMills = tempDate.getTime();
+      tempJson.timeRanges.push(tempTimeMills);
+    }
+    console.log("[WateringConfigs] existGreenHouseIndex: " + existGreenHouseIndex);
+    if (existGreenHouseIndex == -1) {
+      res.json({
+        status: 500,
+        errorMessage: "ไม่สามารถตั้งค่าการให้น้ำของโรงเรือนที่ระบุได้",
+      });
+      return;
+    }
+    configFile.watering[existGreenHouseIndex] = tempJson;
+    await writeConfigFile(configFile);
+    if (writeFileStatus) {
+      res.sendStatus(200);
+    } else {
+      res.json({
+        status: 500,
+        errorMessage: "เกิดข้อผิดพลาดไม่สามารถเขียนไฟล์การตั้งค่าได้"
+      });
     }
   }
 }
@@ -62,22 +75,14 @@ async function getConfigFile() {
 }
 
 function writeConfigFile(configFile, res) {
-  let writeFileResult;
   let content = JSON.stringify(configFile);
   fs.writeFile(String(pathGlobal), content, "utf8", function (err) {
     if (err) {
       console.log(err);
-      writeFileResult = false;
-    }
-    writeFileResult = true;
-    console.log("[WateringConfig] write file with no error");
-    if (writeFileResult) {
-      res.sendStatus(200);
-    } else {
-      res.json({
-        status: 500,
-        errorMessage: "เกิดข้อผิดพลาดไม่สามารถเขียนไฟล์การตั้งค่าได้"
-      });
+      writeFileStatus = false;
+      return;
     }
   });
+  writeFileStatus = true;
+  console.log("[WateringConfig] write file with no error");
 }
