7df130fbd2a29da7c9777f87df16f921865f02de
pon32258 <pon1852@hotmail.com>
2018-08-25 16:05:17 +0700

    [Add]เพิ่ม Route ของ Controller mangement

--- server/routes/controllerManagement.js
+++ server/routes/controllerManagement.js
@@ -1,4 +1,8 @@
 import CreateController from "../classes/ControllerManagement/CreateController";
+import ShowAllGreenHouseController from "../classes/ControllerManagement/ShowAllGreenHouseController";
+import ShowAllProjectController from "../classes/ControllerManagement/ShowAllProjectController";
+import DeleteController from "../classes/ControllerManagement/DeleteController";
+import EditController from "../classes/ControllerManagement/EditController";
 
 const express = require("express");
 const router = express.Router();
@@ -13,8 +17,76 @@ router.use("/createController", (req, res, next) => {
     next();
 });
 
-router.get("/createController", (req, res) => {
+router.post("/createController", (req, res) => {
     new CreateController(req, res);
 });
 
+
+//showAllGreenHouseController
+router.use("/showAllGreenHouseController",(req,res)=>{
+
+    res.setHeader("Access-Control-Allow-Origin",origin_url);
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+    res.set("Content-Type","application/json");
+    next();
+});
+
+router.get("//showAllGreenHouseController",(req,res)=>{
+    new ShowAllGreenHouseController(req,res);
+})
+
+
+//showAllProjectController
+router.use("/showAllProjectController",(req,res)=>{
+
+    res.setHeader("Access-Control-Allow-Origin",origin_url);
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+    res.set("Content-Type","application/json");
+    next();
+});
+
+router.get("//showAllProjectController",(req,res)=>{
+    new ShowAllProjectController(req,res);
+})
+
+
+//DeleteController
+router.use("/deleteController",(req,res)=>{
+
+    res.setHeader("Access-Control-Allow-Origin",origin_url);
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+    res.set("Content-Type","application/json");
+    next();
+});
+
+router.delete("/deleteController",(req,res)=>{
+    new DeleteController(req,res);
+});
+
+
+
+//EditController
+router.use("/editController",(req,res)=>{
+
+    res.setHeader("Access-Control-Allow-Origin",origin_url);
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+    res.set("Content-Type","application/json");
+    next();
+});
+
+router.put("/editController",(req,res)=>{
+    new EditController(req,res);
+});
 module.exports = router;
