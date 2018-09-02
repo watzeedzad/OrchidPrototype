db1d5aba14bad33f8ea419ed6b3eb30783a37d88
Jirawat Boonsumret <jirawat859@hotmail.com>
2018-09-02 16:10:05 +0700

    [update] fix DeleteController class

--- server/classes/ControllerManagement/DeleteController.js
+++ server/classes/ControllerManagement/DeleteController.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const knowController = mongoose.model("know_controller");
 
 let deleteControllerResult;
+let findControllerResult;
 
 export default class CreateController {
     constructor(req, res) {
@@ -11,8 +12,22 @@ export default class CreateController {
     async operation(req, res) {
 
         let macAddress = req.body.macAddress;
-
-        await findAndDeleteController(macAddress);
+        if (typeof macAddress === "undefined") {
+            res.json({
+                status: 500,
+                errorMessage: "macAddress is null"
+            });
+            return;
+        }
+        await findController(macAddress);
+        if (!findControllerResult) {
+            res.json({
+                status: 500,
+                errorMessage: "controller not found!"
+            });
+            return;
+        }
+        await deleteController(macAddress);
         if (deleteControllerResult) {
             res.sendStatus(200);
         } else {
@@ -21,20 +36,31 @@ export default class CreateController {
     }
 }
 
-async function findAndDeleteController(macAddress) {
-    knowController.findOneAndRemove({
-            macAddress: macAddress,
-        },
-        function (err, doc) {
-            if (err) {
-                deleteControllerResult = false;
-                console.log("[deleteControllerResult] findAndDeleteController(err):  " + err);
-            } else if (!doc) {
-                deleteControllerResult = false;
-                console.log("[deleteControllerResult] findAndDeleteController(!doc):  " + doc);
-            } else {
-                deleteControllerResult = true;
-            }
+async function deleteController(macAddress) {
+    await knowController.findOneAndRemove({
+        mac_address: macAddress,
+    }, function (err) {
+        if (err) {
+            deleteControllerResult = false;
+            console.log("[deleteControllerResult] findAndDeleteController (err):  " + err);
+        } else {
+            deleteControllerResult = true;
         }
-    );
+    });
+}
+
+async function findController(macAddress) {
+    await knowController.findOne({
+        mac_address: macAddress
+    }, function (err, result) {
+        if (err) {
+            findControllerResult = false;
+            console.log("[DeleteController] findController (err): " + err);
+        } else if (!result) {
+            findControllerResult = false;
+            console.log("[DeleteController] findController (!result): " + result);
+        } else {
+            findControllerResult = true;
+        }
+    });
 }
