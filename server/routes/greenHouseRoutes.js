74e9cc19ed52274080002f7e9e7eb0c22ab76fc7
Jirawat Boonsumret <jirawat859@hotmail.com>
2018-11-08 23:03:47 +0700

    [update] update multer limit, delete res.setHeader

--- server/routes/greenHouseRoutes.js
+++ server/routes/greenHouseRoutes.js
@@ -9,76 +9,80 @@ const crypto = require("crypto");
 const multer = require("multer");
 const fs = require("fs-extra");
 const storage = multer.diskStorage({
-    destination: function(req, file, callback) {
+    destination: function (req, file, callback) {
         let path = "../OrchidPrototype-Client/public/assets/images"
         fs.mkdirpSync(path);
         callback(null, path);
     },
-    filename: function(req, file, callback) {
+    filename: function (req, file, callback) {
         let hash = crypto.createHash("sha256");
         hash.update(file.fieldname + "-" + Date.now());
         let renameFile = hash.digest("hex") + ".jpg";
         callback(null, renameFile);
     }
 });
-const uploadMulter = multer({storage: storage});
+const uploadMulter = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5000000
+    }
+});
 
-router.use('/addGreenHouse', (req,res,next)=>{
+router.use('/addGreenHouse', (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin_url);
     res.setHeader(
         "Access-Control-Allow-Headers",
         "X-Requested-With,content-type"
     );
-    res.set('Content-Type','application/json');
     next();
 });
 
-router.post('/addGreenHouse', uploadMulter.single('picture'), (req,res)=>{
-    new AddGreenHouse(req,res);
+router.post('/addGreenHouse', uploadMulter.single('picture'), (req, res) => {
+    new AddGreenHouse(req, res);
 });
 
-router.use('/deleteGreenHouse',(req,res,next)=>{
+router.use('/deleteGreenHouse', (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin_url);
     res.setHeader(
         "Access-Control-Allow-Headers",
         "X-Requested-With,content-type"
     );
-    res.set('Content-Type','application/json');
+    res.set('Content-Type', 'application/json');
     next();
 });
 
-router.post('/deleteGreenHouse' ,(req,res)=>{
-    new DeleteGreenHouse(req,res);
+router.post('/deleteGreenHouse', (req, res) => {
+    new DeleteGreenHouse(req, res);
 });
 
 
-router.use('/editGreenHouse', (req,res,next)=>{
+router.use('/editGreenHouse', (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin_url);
     res.setHeader(
         "Access-Control-Allow-Headers",
         "X-Requested-With,content-type"
     );
-    res.set('Content-Type','application/json');
+    res.set('Content-Type', 'application/json');
     next();
 });
 
-router.post('/editGreenHouse', (req,res)=>{
-    new EditGreenHouse(req,res);
+router.post('/editGreenHouse', (req, res) => {
+    new EditGreenHouse(req, res);
 });
 
 
-router.use('/showGreenHouse', (req,res,next)=>{
+router.use('/showGreenHouse', (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin_url);
     res.setHeader(
         "Access-Control-Allow-Headers",
         "X-Requested-With,content-type"
     );
-    res.set('Content-Type','application/json');
+    res.set('Content-Type', 'application/json');
     next();
 });
 
-router.post('/showGreenHouse', (req,res)=>{
-    new ShowGreenHouse(req,res);
+router.post('/showGreenHouse', (req, res) => {
+    new ShowGreenHouse(req, res);
 });
 
 module.exports = router;
