5752032a36c18795dfd513ddaa10673d2fb8bd50
Jirawat Boonsumret <jirawat859@hotmail.com>
2018-12-20 14:24:04 +0700

    [update] fix dependency, delete unused route

--- server/routes/monitoringAndAnalyze.js
+++ server/routes/monitoringAndAnalyze.js
@@ -1,5 +1,3 @@
-import ShowGrowthRateGraph from "../classes/MonitoringAndAnalyze/ShowGrowthRateGraph";
-import ShowSpecificGrowthRateGraph from "../classes/MonitoringAndAnalyze/ShowSpecificGrowthRateGraph";
 import ShowWateringHistory from "../classes/MonitoringAndAnalyze/ShowWateringHistory";
 import ShowFertilizerHistory from "../classes/MonitoringAndAnalyze/ShowFertilizerHistory";
 import AddGrowthRate from "../classes/MonitoringAndAnalyze/AddGrowthRate";
@@ -10,34 +8,6 @@ import GetCompareProject from '../classes/MonitoringAndAnalyze/GetCompareProject
 
 const express = require("express");
 const router = express.Router();
-
-router.use("/showGrowthRateGraph", (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", origin_url);
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-    res.set("Content-Type", "application/json");
-    next();
-});
-
-router.post("/showGrowthRateGraph", (req, res) => {
-    new ShowGrowthRateGraph(req, res);
-});
-
-router.use("/showSpecificGrowthRateGraph", (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", origin_url);
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-    res.set("Content-Type", "application/json");
-    next();
-});
-
-router.post("/showSpecificGrowthRateGraph", (req, res) => {
-    new ShowSpecificGrowthRateGraph(req, res);
-});
 
 router.use("/wateringHistory", (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin_url);
